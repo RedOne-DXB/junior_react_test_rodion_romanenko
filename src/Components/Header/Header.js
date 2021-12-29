3f07054dcde9eb27e733b3f9b9a19019de17be07
Rodion Romanenko <rod.romanenko@gmail.com>
2021-12-29 17:06:46 +0400

    header styles begun but yet to be completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,16 @@
 import HeaderNavTabs from '../HeaderNavTabs/HeaderNavTabs';
+import s from './Header.module.css';
 
 function Header({ children }) {
   return (
-    <header>
-      <HeaderNavTabs />
-      <div>Currency switch</div>
-      <a href="././public/index.html">Basket Button</a>
+    <header className={s.header}>
+      <div className={s.tabContainer}>
+        <HeaderNavTabs />
+        <div className={s.buttonWrapper}>
+          <div>Currency switch</div>
+          <a href="././public/index.html">Basket Button</a>
+        </div>
+      </div>
     </header>
   );
 }
--- src/Components/HeaderNavTabs/HeaderNavTabs.js
+++ src/Components/HeaderNavTabs/HeaderNavTabs.js
@@ -1,9 +1,12 @@
+// import PropTypes from 'prop-types';
+import s from './HeaderNavTabs.module.css';
+
 function HeaderNavTabs() {
   return (
-    <ul>
-      <li>Women</li>
-      <li>Men</li>
-      <li>Kids</li>
+    <ul className={s.tabList}>
+      <li className={s.listItem}>Women</li>
+      <li className={s.listItem}>Men</li>
+      <li className={s.listItem}>Kids</li>
     </ul>
   );
 }
