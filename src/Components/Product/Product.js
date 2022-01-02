c0125c395e6d078db68009406813de0c23bc6f37
Rodion Romanenko <rod.romanenko@gmail.com>
2022-01-02 16:58:13 +0400

    Product list is complete/refactoring to be done when working on creating Classes

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,16 +1,54 @@
 import PropTypes from 'prop-types';
 import s from './Product.module.css';
-// import CartButton from './CartButton';
+import React, { useState } from 'react';
 import CartImage from './Image';
-// import CartButton from './Image';
+// import img1 from './images/emptyCart.svg';
+
+// class Product extends Component {
+
+//   state = {
+//             visible: false,
+//         }
+
+//         show = () => {
+//             this.setState({visible: true});
+//         }
+//         hide = () => {
+//           this.setState({visible: false});
+//         }
+
+// render() {
+
+//   const {url, title, price} = this.props
+
+//   return (
+//     <div className={[s.container]} onMouseEnter={this.show} onMouseLeave={this.hide}>
+//       <a href="./public/index.html" className={s.imageWrapper}>
+//         <img src={url} alt={title} width="320" />
+//       </a>
+//       <CartImage />
+//       <div className={s.content}>
+//         <div className={s.title}>{title}</div>
+//         <div className={s.price}>{price}</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// };
 
 const Product = ({ url, title, price }) => {
+  const [isShown, setIsShown] = useState(false);
   return (
-    <div className={s.container}>
+    <div
+      className={s.container}
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+    >
       <a href="./public/index.html" className={s.imageWrapper}>
         <img src={url} alt={title} width="320" />
       </a>
-      <CartImage />
+      {isShown && <CartImage />}
       <div className={s.content}>
         <div className={s.title}>{title}</div>
         <div className={s.price}>{price}</div>
