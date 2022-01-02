890659742f13e57ee2c90e1bd36bd705ae7ba204
Rodion Romanenko <rod.romanenko@gmail.com>
2022-01-02 18:01:18 +0400

    category page complete/minor things such as paths and hovers to be added

--- src/Components/HeaderNavTabs/HeaderNavTabs.js
+++ src/Components/HeaderNavTabs/HeaderNavTabs.js
@@ -4,9 +4,21 @@ import s from './HeaderNavTabs.module.css';
 function HeaderNavTabs() {
   return (
     <ul className={s.tabList}>
-      <li className={s.listItem}>Women</li>
-      <li className={s.listItem}>Men</li>
-      <li className={s.listItem}>Kids</li>
+      <li className={s.listItem}>
+        <a className={s.linkCurrent} href="./public/index.html">
+          Women
+        </a>
+      </li>
+      <li className={s.listItem}>
+        <a className={s.link} href="./public/index.html">
+          Men
+        </a>
+      </li>
+      <li className={s.listItem}>
+        <a className={s.link} href="./public/index.html">
+          Kids
+        </a>
+      </li>
     </ul>
   );
 }
